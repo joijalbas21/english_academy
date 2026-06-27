cbee2c83de4c9ee83dd6385e1d1681655ce14a8c
jijalbas21 <JIJALBA@soysiglo.21.edu.ar>
2026-06-27 10:36:45 -0300

    Se agregan datos realez al dashboard.

--- src/main/java/com/englishacademy/controllers/DashboardViewController.java
+++ src/main/java/com/englishacademy/controllers/DashboardViewController.java
@@ -1,5 +1,26 @@
 package com.englishacademy.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import com.englishacademy.models.services.AlumnoService;
+import com.englishacademy.models.services.CursoService;
+import com.englishacademy.models.services.ProfesorService;
+
 public class DashboardViewController {
-	// Lógica específica del dashboard (por implementar)
+
+	@FXML
+	private Label labelCursos;
+
+	@FXML
+	private Label labelAlumnos;
+
+	@FXML
+	private Label labelProfesores;
+
+	@FXML
+	public void initialize() {
+		labelCursos.setText(String.valueOf(CursoService.getInstance().obtenerTodos().size()));
+		labelAlumnos.setText(String.valueOf(AlumnoService.getInstance().obtenerTodos().size()));
+		labelProfesores.setText(String.valueOf(ProfesorService.getInstance().obtenerTodos().size()));
+	}
 }
